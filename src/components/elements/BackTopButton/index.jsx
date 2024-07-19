cceebe52c7691e4ad011c125f6c871de5dbb9335
Christian <kristian2015ok@gmail.com>
2024-07-19 11:50:59 +0700

    Fixed: logic display

--- src/components/elements/BackTopButton/index.jsx
+++ src/components/elements/BackTopButton/index.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 
 const BackTopButton = () => {
   const [scrollPosition, setScrollPosition] = useState();
-  const [display, setDisplay] = useState("");
+  const [display, setDisplay] = useState("hidden");
 
   const handleVisibleButton = () => {
     const position = window.pageYOffset;
@@ -36,7 +36,7 @@ const BackTopButton = () => {
         "w-fit h-fit",
         "rounded-md",
         display,
-        "right-5",
+        "right-3",
         "bottom-24 sm:bottom-10 md:bottom-7 lg:bottom-5",
         "z-40",
         "opacity-70"
