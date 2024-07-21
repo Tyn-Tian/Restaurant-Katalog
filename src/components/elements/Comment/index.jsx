d0e6eeca4d1ab606f7deda6aeac4a07d8531e10f
Christian <kristian2015ok@gmail.com>
2024-07-21 23:59:39 +0700

    Style: comment card border

--- src/components/elements/Comment/index.jsx
+++ src/components/elements/Comment/index.jsx
@@ -3,11 +3,11 @@ import "react-loading-skeleton/dist/skeleton.css";
 
 const Comment = ({ data, skeleton }) => {
   return (
-    <div className="bg-gradient-to-r sm:bg-gradient-to-t from-slate-950 to-slate-900 border border-slate-800 p-3 rounded-lg">
+    <div className="bg-transparent border border-white hover:border-red-400 p-3 rounded-lg group transition duration-500">
       <p className="text-white text-lg">
         {skeleton ? <Skeleton count={2} /> : data.review}
       </p>
-      <p className="text-gray-300 mt-3 font-semibold">
+      <p className="text-gray-300 mt-3 font-semibold group-hover:text-red-400 transition duration-500">
         {skeleton ? <Skeleton width={60} /> : data.name}
       </p>
       <p className="text-gray-400 -mt-1">
