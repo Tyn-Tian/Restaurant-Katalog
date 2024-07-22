dfde525de1b94b6f999bf7b0ea9dccdf59f0366d
Christian <kristian2015ok@gmail.com>
2024-07-22 11:06:51 +0700

    Style: Category label

--- src/components/elements/CategoryLabel/index.jsx
+++ src/components/elements/CategoryLabel/index.jsx
@@ -1,7 +1,20 @@
+import { cn } from "../../../utils";
+
 const CategoryLabel = ({ category }) => {
   return (
-    <div className="px-5 py-1 bg-red-400 rounded-md">
-      <p className="text-white font-semibold">{category}</p>
+    <div
+      className={cn(
+        "px-5 py-1",
+        "border border-white hover:border-red-400",
+        "group",
+        "transition duration-500",
+        "bg-transparent",
+        "rounded-md"
+      )}
+    >
+      <p className="text-white font-semibold group-hover:text-red-400 transition duration-500">
+        {category}
+      </p>
     </div>
   );
 };
